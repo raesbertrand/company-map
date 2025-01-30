4ea44e699ade81f2b0a4e9c640c28df61e29f31d
speedkills1 <raes.bertrand@gmail.com>
2025-01-30 20:41:19 +0100

    Async function
    Load api in promise and parse response

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
-var response = await fetch('https://recherche-entreprises.api.gouv.fr/near_point?lat=47.450999&long=-0.555489&radius=1&per_page=25');
-var data = await response.json();
+const api='https://recherche-entreprises.api.gouv.fr/near_point?lat=47.450999&long=-0.555489&radius=1&per_page=25';
 var map = L.map('map').setView([47.468, -0.558], 13);
+
+async function loadApi () {
+	await fetch(api).then(function (response) {
+		return response.json();
+	}).then(function (data) {
+    data.results.forEach((company) => {
+    console.log(company);
+        L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup(company.nom_complet);
+    })
+	});
+}
+loadApi();
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-data.results.forEach((company) => {
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup(company.nom_complet);
-})
