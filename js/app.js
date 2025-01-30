0d83d6f46ee0e96d0654d4b7d84a416e11a3fddd
speedkills1 <raes.bertrand@gmail.com>
2025-01-30 22:17:59 +0100

    loop on api results

--- js/app.js
+++ js/app.js
@@ -1,19 +1,35 @@
-const api='https://recherche-entreprises.api.gouv.fr/near_point?lat=47.450999&long=-0.555489&radius=1&per_page=25';
+const api='https://recherche-entreprises.api.gouv.fr/near_point?lat=47.450999&long=-0.555489&radius=0.5&per_page=25';
 var map = L.map('map').setView([47.468, -0.558], 13);
+var page=1;
+var maxPage=1000;
 
-async function loadApi () {
-	await fetch(api).then(function (response) {
+async function loadApi (url) {
+	await fetch(url).then(function (response) {
 		return response.json();
 	}).then(function (data) {
-    data.results.forEach((company) => {
-    company.matching_etablissements.forEach((etablissement)=>{
-        L.marker([etablissement.latitude, etablissement.longitude]).addTo(map)
-            .bindPopup(company.nom_complet);
-    });
-	});
+  	page++;
+    maxPage=data.total_pages;
+    if(page<=maxPage){
+    	loadApi (api+'&page='+page);
+    }
+    feedMap(data.results);
 });
 }
-loadApi();
+
+function feedMap(companies){ 
+	companies.forEach((company) => {
+    if(company.date_fermeture==null){
+        company.matching_etablissements.forEach((etablissement)=>{
+          if(etablissement.date_fermeture==null){
+              L.marker([etablissement.latitude, etablissement.longitude]).addTo(map)
+                  .bindPopup(company.nom_complet);
+            }
+        });  
+      }
+	});
+}
+
+loadApi(api+'&page='+page);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
