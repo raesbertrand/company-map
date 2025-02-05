20d3263ddb5328b6160316a60821ce6e376a9c38
Bertrand Raes <bertrand.raes@empreintedigitale.fr>
2025-02-05 13:44:06 +0100

    change sql structure

--- src/sql/install.sql
+++ src/sql/install.sql
@@ -1,21 +1,55 @@
-/* Table api_cache {
-  id integer [primary key]
-  source text
-  result text
-  cached_at timestamp 
-}
-*/
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 05 fév. 2025 à 12:39
+-- Version du serveur : 8.0.40
+-- Version de PHP : 8.2.18
 
-CREATE DATABASE company_map;
-USE company_map;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `company_map`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `api_cache`
+--
+
+DROP TABLE IF EXISTS `api_cache`;
 CREATE TABLE IF NOT EXISTS `api_cache` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `source` text COLLATE utf8mb4_0900_as_cs NOT NULL,
-  `result` text COLLATE utf8mb4_0900_as_cs,
+  `source` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `result` json DEFAULT NULL,
   `cached_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `company`
+--
+
+DROP TABLE IF EXISTS `company`;
+CREATE TABLE IF NOT EXISTS `company` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `siret` varchar(14) NOT NULL,
+  `object` json NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
