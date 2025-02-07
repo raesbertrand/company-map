9a3958aa6124a3b97c6b03fcdc7d67a47ab8da52
Bertrand <raes.bertrand@gmail.com>
2025-02-07 21:16:57 +0100

    env var defines in .env file

--- src/inc/config.php
+++ src/inc/config.php
@@ -1,9 +1,3 @@
 <?php
-
-define("DB_HOST", "localhost");
-
-define("DB_USERNAME", "root");
-
-define("DB_PASSWORD", "Empreinte10gitale!");
-
-define("DB_DATABASE_NAME", "company_map");
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
