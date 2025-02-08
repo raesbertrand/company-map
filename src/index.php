235e4f83591a072de67709c38722500caa43c3b8
Bertrand <raes.bertrand@gmail.com>
2025-02-08 14:47:40 +0100

    fix api control

--- src/index.php
+++ src/index.php
@@ -5,11 +5,21 @@ $uri = explode( '/', $uri );
 $uriSize=sizeOf($uri);
 $module=$uri[$uriSize-2];
 $param=$uri[$uriSize-1];
-if ((isset($module) && $module != 'company') || !isset($param)) {
+
+
+/* check if module is allowed */
+if ((isset($module) && 
+    $module != 'company'
+    &&
+    $module != 'cache'
+    )
+     || !isset($param)) {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
+
 require PROJECT_ROOT_PATH . "/Controller/Api/CompanyController.php";
+require PROJECT_ROOT_PATH . "/Controller/Api/CacheController.php";
 $objFeedController = new CompanyController();
 $strMethodName = $param . 'Action';
 $objFeedController->{$strMethodName}();
