d92df2fac0916e4cf328fe667160aa3cb683b6cf
Bertrand <raes.bertrand@gmail.com>
2025-02-08 14:45:45 +0100

    search companies methods

--- src/Controller/Api/CompanyController.php
+++ src/Controller/Api/CompanyController.php
@@ -1,9 +1,9 @@
 <?php
 class CompanyController extends BaseController
 {
-/**
-* "/company/list" Endpoint - Get list of companies
-*/
+    /**
+     * "/company/list" Endpoint - Get list of companies
+     */
     public function listAction()
     {
         $strErrorDesc = '';
@@ -19,7 +19,7 @@ class CompanyController extends BaseController
                 $arrCompanies = $companyModel->getCompanies($intLimit);
                 $responseData = json_encode($arrCompanies);
             } catch (Error $e) {
-                $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
+                $strErrorDesc = $e->getMessage() . 'Something went wrong! Please contact support.';
                 $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
             }
         } else {
@@ -27,13 +27,62 @@ class CompanyController extends BaseController
             $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
         }
         // send output
+        $this->companySendOutput($strErrorDesc, $responseData, $strErrorHeader);
+    }
+
+    public function searchAction($lat, $long, $distance)
+    {
+        /* SELECT 
+        -- stuff here
+        , ( 6371000 * acos( cos( radians(45.815005) ) * cos( radians( stuff.lat ) ) * cos( radians( stuff.lng ) - radians(15.978501) ) + sin( radians(45.815005) ) * sin(radians(stuff.lat)) ) ) AS distance 
+        FROM 
+        stuff
+        HAVING 
+        distance < 500 */
+        $sql = 'SELECT 
+        * , ( 6371000 * acos( cos( radians(45.815005) ) * cos( radians( ' . $lat . ' ) ) * cos( radians( ' . $long . ' ) - radians(15.978501) ) + sin( radians(45.815005) ) * sin(radians(stuff.lat)) ) ) AS distance 
+        FROM 
+        company
+        HAVING 
+        distance < ' . $distance;
+
+
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+
+        if (!isset($arrQueryStringParams['lat']) || !isset($arrQueryStringParams['long']) || !isset($arrQueryStringParams['distance'])) {
+            $strErrorDesc = 'Missing params';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+        try {
+            $companyModel = new CompanyModel();
+            $intLimit = 10;
+            if (isset($arrQueryStringParams['limit']) && $arrQueryStringParams['limit']) {
+                $intLimit = $arrQueryStringParams['limit'];
+            }
+            $arrCompanies = $companyModel->getCompanysAround($arrQueryStringParams['lat'],$arrQueryStringParams['long'],$arrQueryStringParams['distance']);
+            $responseData = json_encode($arrCompanies);
+        } catch (Error $e) {
+            $strErrorDesc = $e->getMessage() . 'Something went wrong! Please contact support.';
+            $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
+        }
+
+        // send output
+        $this->companySendOutput($strErrorDesc, $responseData, $strErrorHeader);
+    }
+
+    function companySendOutput($strErrorDesc, $responseData, $strErrorHeader)
+    {
+        // send output
         if (!$strErrorDesc) {
             $this->sendOutput(
                 $responseData,
                 array('Content-Type: application/json', 'HTTP/1.1 200 OK')
             );
         } else {
-            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+            $this->sendOutput(
+                json_encode(array('error' => $strErrorDesc)),
                 array('Content-Type: application/json', $strErrorHeader)
             );
         }
--- src/Model/CompanyModel.php
+++ src/Model/CompanyModel.php
@@ -6,4 +6,12 @@ class CompanyModel extends Database
     {
         return $this->select("SELECT * FROM company ORDER BY id ASC LIMIT ?", ["i", $limit]);
     }
+
+    public function getCompanysAround($lat,$long,$distance){
+        // TODO search in cache : and refresh it if necessary
+
+        // return cached values
+        $dbResult=$this->select("SELECT * , ( 6371000 * acos( cos( radians(45.815005) ) * cos( radians( ? ) ) * cos( radians( ? ) - radians(15.978501) ) + sin( radians(45.815005) ) * sin(radians(stuff.lat)) ) ) AS distance FROM company HAVING distance < ?", ["i", $lat, $long, $distance]);
+        return $dbResult;
+    }
 }
