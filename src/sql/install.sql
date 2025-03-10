1171ffaccc1e44a48cb732c6cc0341cbbe8448bb
Bertrand Raes <raes.bertrand@gmail.com>
2025-03-10 13:45:59 +0100

    add created_at column

--- src/sql/install.sql
+++ src/sql/install.sql
@@ -62,11 +62,13 @@ COMMIT;
 -- Structure de la table `company_notes`
 --
 
+
 DROP TABLE IF EXISTS `company_notes`;
 CREATE TABLE IF NOT EXISTS `company_notes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `siret` int NOT NULL,
   `note` text NOT NULL,
+  `created_at` timestamp NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
