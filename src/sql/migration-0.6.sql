92d0ff354ae1be3ea0740b4d5e9f9367f88a5091
Bertrand Raes <raes.bertrand@gmail.com>
2025-03-10 13:51:22 +0100

    created_at default value as current timestamp

--- src/sql/migration-0.6.sql
+++ src/sql/migration-0.6.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `company_notes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `siret` int NOT NULL,
   `note` text NOT NULL,
-  `created_at` timestamp NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
