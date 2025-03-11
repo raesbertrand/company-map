97e609e1ddd09407e6dc5c5c5bbdd2958fd1da50
Bertrand Raes <raes.bertrand@gmail.com>
2025-03-11 14:07:10 +0100

    declare note class

--- src/index.php
+++ src/index.php
@@ -6,9 +6,12 @@ $uriSize=sizeOf($uri);
 $module=$uri[$uriSize-2];
 $param=$uri[$uriSize-1];
 
+$allowedModules=['company', 'cache', 'note'];
+
 /* check if module is allowed */
 if (
-    (isset($module) && $module != 'company' && $module != 'cache')
+    // (isset($module) && $module != 'company' && $module != 'cache')
+    (isset($module) && array_search($module, $allowedModules)===false)
      || 
      !isset($param)) {
     header("HTTP/1.1 404 Not Found");
@@ -18,10 +21,7 @@ if (
 
 require PROJECT_ROOT_PATH . "/Controller/Api/CompanyController.php";
 require PROJECT_ROOT_PATH . "/Controller/Api/CacheController.php";
-/*
-$objFeedController = new CompanyController();
-$objFeedController = new CacheController();
-*/
+require PROJECT_ROOT_PATH . "/Controller/Api/NoteController.php";
 
 
 $className = ucfirst($module) . 'Controller';
